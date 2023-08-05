77c8fba53038b4fc1ea4ed3c57d0a4fd91f179be
Sebastian Pałucha <sebastian.palucha@york.ac.uk>
2023-08-05 16:42:04 +0100

    Clean GH source code
    Remove cached build directory ./target/**/* directory ```git rm -r --cached target```
    Add chmod +x mvnm
    Add .gitignore and ignore all target ./target directory
    Move spring-boot logfile to target folder

--- target/classes/data.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO Author(id, forename, surname) VALUES(100001, 'Nilani', 'Ganeshwaran');
-INSERT INTO Author(id, forename, surname) VALUES(100002, 'Berrisford', 'Edwards');
-INSERT INTO Author(id, forename, surname) VALUES(100003, 'Ian', 'Gifford');
